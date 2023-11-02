32e0254425adefc07ab13324d9e676698da28e2e
Hingaz <hhinga04@gmail.com>
2023-11-02 12:02:49 +0300

    Complete assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setInCart] = useState(false);
+
+  const toggleCartStatus = () => {
+    setInCart(!isInCart);
   return (
-    <li className="">
+    <li className={isInCart ? 'in-cart' : ''}>
+
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={toggleCartStatus}>
+        {isInCart ? 'Remove From Cart' : 'Add to Cart'}
+      </button>
     </li>
   );
+}
+
 }
 
 export default Item;
